373e2bdb3f4491b6da0b5774e66059ae7ddf1971
jlee-prog <69129120+jlee-prog@users.noreply.github.com>
2020-08-07 22:43:05 -0400

    Create Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,15 @@
+public class Datatype{
+	public static void main(String[] args) {
+		System.out.println(5); // Number
+		System.out.println("five"); // String
+		
+		System.out.println("5"); // String 5
+		
+		System.out.println(5+5); // 10
+		System.out.println("5"+"5");
+		
+		System.out.println(5*5); // 25
+		
+		System.out.println("1111".length()); //4 글자길이 
+	}
+}
